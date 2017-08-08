0c34597681a2c2e07c164c0fa6e641e2accc9606
IkaRus <kokoj2@yandex.ru>
2017-08-08 17:41:41 +0300

    Add basic sound staff and some questions.

--- Controller/audiocontroller.cpp
+++ Controller/audiocontroller.cpp
@@ -2,16 +2,35 @@
 
 AudioController::AudioController(QObject *parent) : QObject(parent)
 {
+    QAudioFormat audioFormat;
+    audioFormat.setSampleRate(8000);
+    audioFormat.setChannelCount(1);
+    audioFormat.setSampleSize(8);
+    audioFormat.setCodec("audio/pcm");
+    audioFormat.setByteOrder(QAudioFormat::LittleEndian);
+    audioFormat.setSampleType(QAudioFormat::UnSignedInt);
 
+    audioInput = new QAudioInput(audioFormat,this);
+    audioOutput = new QAudioOutput(audioFormat,this);
+
+    QAudioDeviceInfo inputDevice = QAudioDeviceInfo::defaultInputDevice();
+    QAudioDeviceInfo outputDevice = QAudioDeviceInfo::defaultOutputDevice();
 }
 
 QByteArray AudioController::getDataFromDevice()  //YARIK::ATTENTION: implement this method, it should return data readed from input device (mic, for example)
 {
-    return QByteArray();
+    inputAudioBuffer.open(QBuffer::ReadWrite);
+    inputAudioBuffer.seek(0);
+    audioInput->start(&inputAudioBuffer); //ANTON::ATTENTION:: when to stop writing data and return them?
+    return QByteArray(inputAudioBuffer);
 }
 
 void AudioController::pushDataToOutput(const Room &room) //YARIK::ATTENTION: implement this method. It should run over every user in room, get it's audio data and out it to device (dynamics for example)
 {
 
+    outputAudioBuffer.open(QBuffer::ReadWrite);
+    outputAudioBuffer.seek(0);
+    outputAudioBuffer.setData(); //ANTON::ATTENTION:: Where to get data? From the previous class? If yes, how?
+    audioOutput->start(&outputBuffer);
 }
 
--- Controller/audiocontroller.h
+++ Controller/audiocontroller.h
@@ -4,6 +4,9 @@
 #include <QObject>
 #include <QBuffer>
 #include <Model/room.h>
+#include <QAudioInput> //ANTON::ATTENTION:: QAudioInput/Output connected with QObject, isn't it?
+#include <QAudioOutput>
+#include <QDebug>
 
 class AudioController : public QObject //YARIK::ATTENTION: This is your main class, create and store all needed objects here
 {
@@ -13,7 +16,13 @@ public:
     QByteArray getDataFromDevice();
     void pushDataToOutput(const Room& room);
 signals:
-
+private:
+    QBuffer inputAudioBuffer;
+    QAudioInput *audioInput;
+    QAudioDeviceInfo inputDevice;
+    QBuffer outputAudioBuffer;
+    QAudioInput *audioOutput;
+    QAudioDeviceInfo outputDevice;
 public slots:
 };
 
